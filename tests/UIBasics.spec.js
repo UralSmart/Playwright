44b4f8fc7272011ce7eb48d6b6f77c11f3b7e539
Alexey Pavlov <uralsmart@gmail.com>
2022-12-04 17:35:44 +0400

    UI basics

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -3,23 +3,35 @@ const {test, expect} = require('@playwright/test')
 
 test('Browser context', async ({browser}) =>
 {
-    const contex = await browser.newContext() //incognito like
+    const contex = await browser.newContext()
     const page = await contex.newPage()
     const userName = page.locator("#username")
     const userPass = page.locator("#password")
     const signIn = page.locator("#signInBtn")
+    const cardTitles = page.locator(".card-body a")
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
     console.log(await page.title())
-    // css
-    await userName.type("aaa")
-    await userPass.type("learning")
-    await signIn.click()
-    console.log(await page.locator("[style*='block']").textContent(""))
-    await expect(page.locator("[style*='block']")).toContainText("Incorrect")
-    // type - fill
-    await userName.fill("")
+    // // wrong username
+    // await userName.type("aaa")
+    // await userPass.type("learning")
+    // await signIn.click()
+    // console.log(await page.locator("[style*='block']").textContent(""))
+    // await expect(page.locator("[style*='block']")).toContainText("Incorrect")
+    // // wrong password
+    // await userName.fill("")
+    // await userPass.fill("")
+    // await userName.type("rahulshettyacademy")
+    // await userPass.type("aaa")
+    // await signIn.click()
+    // console.log(await page.locator("[style*='block']").textContent(""))
+    // await expect(page.locator("[style*='block']")).toContainText("Incorrect")
+    // // correct credentials
+    // await userName.fill("")
+    // await userPass.fill("")
     await userName.fill("rahulshettyacademy")
+    await userPass.fill("learning")
     await signIn.click()
-    // console.log(await page.locator(".card-body a").textContent())
+    const allTitles = await cardTitles.allTextContents()
+    console.log(allTitles)
 })
 
