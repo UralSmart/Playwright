f5275d063062c3e9680a7c794b304a89bf9d20ea
Alexey Pavlov <uralsmart@gmail.com>
2022-12-04 18:32:13 +0400

    client app

---
+++ tests/ClientApp.spec.js
@@ -0,0 +1,16 @@
+const {test, expect} = require('@playwright/test')
+
+
+test.only('Browser context', async ({page}) =>
+{
+    // login
+    await page.goto("https://rahulshettyacademy.com/client/")
+    await page.locator("#userEmail").fill("uralsmart@gmail.com")
+    await page.locator("#userPassword").fill("12345678rA")
+    await page.locator("#login").click()
+    await page.waitForLoadState("networkidle")
+    // list of goods
+    const titles = await page.locator(".card-body b").allTextContents()
+    console.log(titles)
+})
+
--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -10,24 +10,14 @@ test('Browser context', async ({browser}) =>
     const signIn = page.locator("#signInBtn")
     const cardTitles = page.locator(".card-body a")
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
-    console.log(await page.title())
-    // // wrong username
-    // await userName.type("aaa")
-    // await userPass.type("learning")
-    // await signIn.click()
-    // console.log(await page.locator("[style*='block']").textContent(""))
-    // await expect(page.locator("[style*='block']")).toContainText("Incorrect")
-    // // wrong password
-    // await userName.fill("")
-    // await userPass.fill("")
-    // await userName.type("rahulshettyacademy")
-    // await userPass.type("aaa")
-    // await signIn.click()
-    // console.log(await page.locator("[style*='block']").textContent(""))
-    // await expect(page.locator("[style*='block']")).toContainText("Incorrect")
-    // // correct credentials
-    // await userName.fill("")
-    // await userPass.fill("")
+    // wrong username
+    await userName.type("aaa")
+    await userPass.type("learning")
+    await signIn.click()
+    console.log(await page.locator("[style*='block']").textContent(""))
+    await expect(page.locator("[style*='block']")).toContainText("Incorrect")
+    // correct credentials
+    await userPass.fill("")
     await userName.fill("rahulshettyacademy")
     await userPass.fill("learning")
     await signIn.click()
