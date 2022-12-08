5ebf89ce6223dcb2ef69511c8c25c479e17cfe50
Alexey Pavlov <uralsmart@gmail.com>
2022-12-08 19:17:08 +0400

    Add to a cart

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -3,6 +3,8 @@ const {test, expect} = require('@playwright/test')
 
 test('Browser context', async ({page}) =>
 {
+    const productName = "Zara Coat 4"
+    const products = page.locator(".card-body")
     // login
     await page.goto("https://rahulshettyacademy.com/client/")
     await page.locator("#userEmail").fill("uralsmart@gmail.com")
@@ -12,5 +14,14 @@ test('Browser context', async ({page}) =>
     // list of goods
     const titles = await page.locator(".card-body b").allTextContents()
     console.log(titles)
+    // Zara coat 4
+    const count = products.count()
+    for(let i = 0; i < count; i++) {
+        if(await products.nth(i).locator("b").textContent() === productName) {
+            // add to a cart
+            await products.nth(i).locator("text= Add to card").click()
+            break
+        }
+    }
 })
 
