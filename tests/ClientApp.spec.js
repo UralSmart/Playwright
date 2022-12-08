b66012d8aac0484bec9c1a1cd4bf729f0593e280
Alexey Pavlov <uralsmart@gmail.com>
2022-12-08 20:03:14 +0400

    check the cart

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -3,7 +3,7 @@ const {test, expect} = require('@playwright/test')
 
 test('Browser context', async ({page}) =>
 {
-    const productName = "Zara Coat 4"
+    const productName = "zara coat 3"
     const products = page.locator(".card-body")
     // login
     await page.goto("https://rahulshettyacademy.com/client/")
@@ -15,13 +15,18 @@ test('Browser context', async ({page}) =>
     const titles = await page.locator(".card-body b").allTextContents()
     console.log(titles)
     // Zara coat 4
-    const count = products.count()
+    const count = await products.count()
     for(let i = 0; i < count; i++) {
         if(await products.nth(i).locator("b").textContent() === productName) {
             // add to a cart
-            await products.nth(i).locator("text= Add to card").click()
+            await products.nth(i).locator("text= Add To Cart").click()
             break
         }
     }
+    // check cart
+    await page.locator("[routerlink*='cart']").click()
+    await page.locator("div li").first().waitFor()
+    const bool = await page.locator("h3:has-text('zara coat 3')").isVisible()
+    expect(bool).toBeTruthy()
 })
 
