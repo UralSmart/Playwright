1bde958b954b6efd67b63ae705809ce1e1016a48
Alexey Pavlov <uralsmart@gmail.com>
2022-12-10 19:39:52 +0400

    how to find locator

---
+++ tests/How to find attribute.js
@@ -0,0 +1,22 @@
+/*
+
+
+
+If ID is presented
+css -> tagname#id (or) #id
+
+If class attribute is presented
+css -> tagname.class (or) .class
+
+Write css based on any attribute
+css -> [attribute='value']
+
+Write css with traversing from parent to child
+css -> parenttagname childtagname
+
+If needed to write the locator based on text
+text=''
+
+
+
+*/
