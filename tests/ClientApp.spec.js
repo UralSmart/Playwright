7538770a2c23b04e8832ed56a06d359634c70fd1
Alexey Pavlov <uralsmart@gmail.com>
2022-12-11 19:19:33 +0400

    order details

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -5,9 +5,10 @@ test('Browser context', async ({page}) =>
 {
     const productName = "zara coat 3"
     const products = page.locator(".card-body")
+    const email = "uralsmart@gmail.com"
     // login
     await page.goto("https://rahulshettyacademy.com/client/")
-    await page.locator("#userEmail").fill("uralsmart@gmail.com")
+    await page.locator("#userEmail").fill(email)
     await page.locator("#userPassword").fill("12345678rA")
     await page.locator("#login").click()
     await page.waitForLoadState("networkidle")
@@ -41,6 +42,26 @@ test('Browser context', async ({page}) =>
             break
         }
     }
-    await page.pause()
+    await expect(page.locator("label[type='text']")).toHaveText(email)
+    await page.locator(".action__submit").click()
+    await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order.")
+    let orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent()
+    orderId = orderId.substring(2, 26)
+    console.log(orderId)
+    // order list
+    await page.locator("button[routerlink*='myorders']").click()
+    await page.locator("tbody").waitFor()
+    const rows = await page.locator("tbody tr")
+    for(let i = 0; i < await rows.count; i++) {
+        const rowOrderId = await rows.nth(i).locator("th").textContent
+        if(orderId.includes(rowOrderId)) {
+            await rows.nth(i).locator("button").first().click()
+            console.log(rowOrderId)
+            break
+        }
+    }
+    // const orderIdDetails = await page.locator(".col-text.-main").textContent()
+    // console.log(orderIdDetails)
+    // expect(orderId.includes(orderIdDetails)).toBeTruthy()
 })
 
