b0b98f33a884308663a62a8f0b0a4661a7048812
Alexey Pavlov <uralsmart@gmail.com>
2022-12-12 20:51:36 +0400

    events, frames

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -8,5 +8,13 @@ test("Popup validations", async({page}) =>
     await expect(page.locator("#displayed-text")).toBeVisible()
     await page.locator("#hide-textbox").click()
     await expect(page.locator("#displayed-text")).toBeHidden()
-
+    // events
+    page.on("dialog", dialog => dialog.accept()) // or dismiss
+    await page.locator("#confirmbtn").click()
+    await page.locator("#mousehover").hover()
+    // frames
+    const framesPage = page.frameLocator("#courses-iframe")
+    await framesPage.locator("li a[href*='lifetime-access']:visible").click()
+    const textCheck = await framesPage.locator(".text h2").textContent()
+    console.log(textCheck.split(" ")[1])
 })
